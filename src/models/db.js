084697f28ebe34a5fd5678cf43e7ad36390d11d3
Karl Rohr <karlrohr@annihilation.local>
2016-06-03 22:21:49 -0400

    added dotenv to dev dependencies and require it in db.js

--- src/models/db.js
+++ src/models/db.js
@@ -1,2 +1,3 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS);
+require('dotenv').config();
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {});
