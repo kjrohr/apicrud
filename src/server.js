eb00009396fcd744ac455f4b93875923d534d2cb
Karl Rohr <karlrohr@annihilation.local>
2016-06-07 18:09:56 -0400

    Fixed partial issues with pathing and how the util file works.

--- src/server.js
+++ src/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const util = require('../lib/util.js');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: true,
 }));
 
 app.use('/', require('./routes')(express));
 
-exports.server = app.listen(port, ()=>{
-  console.log("Server is running on port: " + port);
+exports.server = app.listen(port, () => {
+  util.debug('Server is running on port: ' + port);
 });
